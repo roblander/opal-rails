e3574300af3485560de3d3097d3af1030abad2af
Anton Zhuravsky <mail2lf@gmail.com>
2013-02-03 00:12:15 +0300

    Fixing options for opal specs

--- spec/integration/in_browser_specs_spec.rb
+++ spec/integration/in_browser_specs_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'In-browser specs runner' do
-  it 'runs all specs', :js do
+  it 'runs all specs', :js => true do
     visit '/opal_spec'
     page.should have_content('Running: spec')
     page.should have_content('1 examples, 0 failures')
