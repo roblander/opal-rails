8c8638b42cd382c808bb6cd90584c4746a92750f
Anton Zhuravsky <mail2lf@gmail.com>
2013-02-23 19:59:52 +0300

    Fixing opal-rails engie

--- lib/opal/rails/engine.rb
+++ lib/opal/rails/engine.rb
@@ -3,11 +3,20 @@ require 'rails'
 module Opal
   module Rails
     class Engine < ::Rails::Engine
+      
       config.app_generators.javascript_engine :opal
 
-      # Cache eager_load_paths now, otherwise the assets dir is added
-      # and its .rb files are eagerly loaded.
-      config.autoload_paths
+      config.eager_load_paths
+
+      initializer 'opal.asset_paths', :after => 'sprockets.environment', :group => :all do |app|
+        app.config.before_initialize do
+          app.config.eager_load_paths = app.config.eager_load_paths.dup - Dir["#{app.root}/app/{assets,views}"]
+        end
+
+        Opal.paths.each do |path|
+          app.assets.append_path path
+        end
+      end
 
     end
   end
